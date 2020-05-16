a6404d21167524181947ada03030dca8ebc7cf52
vinaysharmaqss <vinay.sharma@qsstechnologies.com>
2020-05-16 17:53:48 +0530

    material ui drawer component used and routers implemented on side drawer

--- Crud operation/client/assignment_app/src/App.js
+++ Crud operation/client/assignment_app/src/App.js
@@ -5,11 +5,32 @@ import "./App.css";
 // import { string } from 'prop-types';
 // import { Tab } from '@material-ui/core';
 // import Assignments from "./containers/Assignments/Assignments";
-import Student from "./containers/Students/Student";
+import Students from "./containers/Students/Student";
+import Drawer from "./components/Drawer";
+import Assignments from "./containers/Assignments/Assignments";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Home = () => {};
 
 class App extends Component {
   render() {
-    return <Student />;
+    return (
+      <BrowserRouter>
+        <Drawer>
+          <Switch>
+            {/* switch provides kind of typical switch case alternatives similar to regular ones. just providing options which path to called for a key/ component */}
+            <Route component={Assignments} path={"/Assignments"} />
+            <Route component={Students} path={"/Students"} />
+            <Route
+              render={() => {
+                return <h1>"Hello to my default route"</h1>;
+              }}
+              path={"/"}
+            />
+          </Switch>
+        </Drawer>
+      </BrowserRouter>
+    );
   }
 }
 export default App;
